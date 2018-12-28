f90c7e492bf3ede393238c8e901614125d82559e
DJMF <djm.fulton@gmail.com>
2018-12-28 18:13:38 +0000

    Update chart_helper.js

--- src/helpers/chart_helper.js
+++ src/helpers/chart_helper.js
@@ -10,23 +10,4 @@ const ChartHelper = function(words) {
   }
   return wordData;
 }
-// var pattern  = words.split(/[,\. ]+/g)
-// matchedWords = words.match(pattern);
-// const counts = matchedWords.reduce((accumulator, word) => {
-//   return accumulator + word;
-// }, 0);
-//
-// if ( words.hasOwnProperty( word ) ) {
-//   words[ word ] = words[ word ] + 1;
-// } else {
-//   /* `stats` does not yet have an entry for the current `word`.
-//   As a result, let's add a new entry, and set count to 1. */
-//   words[ word ] = 1;
-// }
-// return words;
-
-// /* Now that `counts` has our object, we can log it. */
-// console.log( counts );
-// // ());
-// };
 module.exports = ChartHelper;
